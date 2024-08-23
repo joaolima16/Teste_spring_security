8924e5bf2c9a32f6a0da8d3756e7e48113d71da0
João Vitor De Lima <88281625+joaolima16@users.noreply.github.com>
2024-08-23 15:53:45 -0300

    Add files via upload

---
+++ src/main/java/com/pi/yamam/domain/user/User.java
@@ -0,0 +1,62 @@
+package com.pi.yamam.domain.user;
+
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Column(unique = true)
+    private String cpf;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private UserStatus status;
+
+    private UserRoles role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        if (this.role == UserRoles.ADMIN)
+            return  List.of(new SimpleGrantedAuthority("ROLE_ADMIN"), new SimpleGrantedAuthority("ROLE_STOCKIST"));
+        else
+            return  List.of(new SimpleGrantedAuthority(("ROLE_STOCKIST")));
+    }
+
+    @Override
+    public String getUsername() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getUsername'");
+    }
+
+}
---
+++ src/main/java/com/pi/yamam/repositories/UserRepository.java
@@ -0,0 +1,12 @@
+package com.pi.yamam.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.pi.yamam.domain.user.User;
+import java.util.List;
+
+
+public interface UserRepository extends JpaRepository<User, Long>{
+    UserDetails findByCpf(String cpf);
+}
